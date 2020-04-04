f829d9a5dd1ea5598064ffcf074f38afe3740476
Alexandre Lacerda <alexandre.lacerda36@gmail.com>
2020-04-04 09:53:57 -0400

    entrada de dados de 2 numeros + operação soma.

---
+++ CalculoTeste/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CalculoTeste {
+    class Program {
+        static void Main(string[] args) {
+
+            // entrada de dados
+            Console.WriteLine("Entre com dois números para as operações ");
+            Console.Write("digite o primeiro: ");
+            double n1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Digite o segundo número: ");
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // operações ( processamento )
+            double soma = n1 + n2;
+
+
+            // saída de dados
+            Console.WriteLine("soma =  " + soma.ToString("f2",CultureInfo.InvariantCulture));
+        }
+    }
+}
